d4ddab410444c8762fe14b764bcb017301f6a37d
Ben Jyring <benjyring@gmail.com>
2018-01-16 16:57:30 -0600

    Slightly drier to store randomness in the class

--- game.js
+++ game.js
@@ -1,18 +1,20 @@
 function Hexagon(name,a,b,c,d,e,f){
 	this.name = name;
-	this.a = a;
-	this.b = b;
-	this.c = c;
-	this.d = d;
-	this.e = e;
-	this.f = f;
+	this.a = a || rand(1, 6);
+	this.b = b || rand(1, 6);
+	this.c = c || rand(1, 6);
+	this.d = d || rand(1, 6);
+	this.e = e || rand(1, 6);
+	this.f = f || rand(1, 6);
 }
+
 function rand(min,max){
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
+
 var totalHexes = Math.pow(6,6);
 var hexArray = [];
 
 for (var i = 0; i < totalHexes; i++) {
-	hexArray.push(new Hexagon(('hex'+i),rand(1, 6),rand(1, 6),rand(1, 6),rand(1, 6),rand(1, 6),rand(1, 6)));
+	hexArray.push(new Hexagon('hex'+i));
 }
