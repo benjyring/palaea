40d76b989293c0787b2803f536f617cc44df3eaf
Ben Jyring <benjyring@gmail.com>
2018-11-19 19:42:50 -0600

    WFH obstacle detection

--- js/components/innerMap.js
+++ js/components/innerMap.js
@@ -1,3 +1,7 @@
+function getTileByXY(cell, tileX, tileY){
+	return cell.inner.filter(tile => tile.x == tileX && tile.y == tileY)[0];
+}
+
 function generateTiles(){
 	for (y=0; y<app.totalY; y++){
 		for (x=0; x<app.totalX; x++){
@@ -25,15 +29,24 @@ function drawTiles(d){
 			if ((el.x)*app.inner.sideLen > app.gameArea.offsetX){
 				if (el.o === true){
 					app.innerCtx.fillStyle = 'darkgrey';
-					app.innerCtx.fillRect(((el.x*d)-app.gameArea.offsetX), ((el.y*d)-app.gameArea.offsetY), d, d);
+					app.innerCtx.fillRect((((el.x-1)*d)-app.gameArea.offsetX), (((el.y-1)*d)-app.gameArea.offsetY), d, d);
 					app.innerCtx.strokeStyle = 'black';
-					app.innerCtx.strokeRect(((el.x*d)-app.gameArea.offsetX), ((el.y*d)-app.gameArea.offsetY), d, d);
+					app.innerCtx.strokeRect((((el.x-1)*d)-app.gameArea.offsetX), (((el.y-1)*d)-app.gameArea.offsetY), d, d);
 				} else {
 					app.innerCtx.imageSmoothingEnabled = false;
-					app.innerCtx.fillStyle = app.myPop.cell.env.color;
-					app.innerCtx.fillRect(((el.x*d)-app.gameArea.offsetX), ((el.y*d)-app.gameArea.offsetY), d, d);
+					// app.innerCtx.fillStyle = app.myPop.cell.env.color;
+					// app.innerCtx.fillRect((((el.x-1)*d)-app.gameArea.offsetX), (((el.y-1)*d)-app.gameArea.offsetY), d, d);
+
+					app.innerCtx.drawImage(
+						document.getElementById(app.myPop.cell.env.type),
+						(((el.x-1)*d)-app.gameArea.offsetX),
+						(((el.y-1)*d)-app.gameArea.offsetY),
+						d,
+						d
+					);
+
 					app.innerCtx.strokeStyle = 'black';
-					app.innerCtx.strokeRect(((el.x*d)-app.gameArea.offsetX), ((el.y*d)-app.gameArea.offsetY), d, d);
+					app.innerCtx.strokeRect((((el.x-1)*d)-app.gameArea.offsetX), (((el.y-1)*d)-app.gameArea.offsetY), d, d);
 				}
 			}
 		}
@@ -43,6 +56,32 @@ function drawTiles(d){
 function drawChar(d){
 	app.innerCtx.fillStyle = 'red';
 	app.innerCtx.fillRect((app.innerMap.width/2),(app.innerMap.height/2), d, d);
+}
+
+function getCharTile(offsetX, offsetY){
+	var halfCharLen = app.inner.sideLen/4,
+		halfScreenHeight = app.innerMap.height/2,
+		halfScreenWidth = app.innerMap.width/2,
+		tileOffsetX = Math.round(offsetX/app.inner.sideLen),
+		tileOffsetY = Math.round(offsetY/app.inner.sideLen);
+
+	return getTileByXY(
+		app.myPop.cell,
+		Math.floor((halfScreenWidth + halfCharLen)/app.inner.sideLen)+ tileOffsetX + 1,
+		Math.floor((halfScreenHeight + halfCharLen)/app.inner.sideLen) + tileOffsetY + 1
+	);
+}
+
+function detectObstacle(){
+	// TO DO
+	// INCOMPLETE
+	var obstacle = false;
+
+	if (getPlayerTile().o === true){
+		obstacle = true;
+	}
+
+	return obstacle;
 }
 
 function startGame(){
@@ -62,16 +101,36 @@ function updateGameArea(){
 	app.gameArea.clear();
 
 	if (app.gameArea.keys && app.gameArea.keys[65]){
-		app.gameArea.offsetX -= app.inner.moveBy;
+		var innerOffsetX = app.gameArea.offsetX - app.inner.moveBy,
+			innerOffsetY = app.gameArea.offsetY;
+
+		if (getCharTile(innerOffsetX, innerOffsetY).o === false){
+			app.gameArea.offsetX = innerOffsetX;
+		}
 	}
 	if (app.gameArea.keys && app.gameArea.keys[68]){
-		app.gameArea.offsetX += app.inner.moveBy;
+		var innerOffsetX = app.gameArea.offsetX + app.inner.moveBy,
+			innerOffsetY = app.gameArea.offsetY;
+
+		if (getCharTile(innerOffsetX, innerOffsetY).o === false){
+			app.gameArea.offsetX = innerOffsetX;
+		}
 	}
 	if (app.gameArea.keys && app.gameArea.keys[87]){
-		app.gameArea.offsetY -= app.inner.moveBy;
+		var innerOffsetX = app.gameArea.offsetX,
+			innerOffsetY = app.gameArea.offsetY - app.inner.moveBy;
+
+		if (getCharTile(innerOffsetX, innerOffsetY).o === false){
+			app.gameArea.offsetY = innerOffsetY;
+		}
 	}
 	if (app.gameArea.keys && app.gameArea.keys[83]){
-		app.gameArea.offsetY += app.inner.moveBy;
+		var innerOffsetX = app.gameArea.offsetX,
+			innerOffsetY = app.gameArea.offsetY + app.inner.moveBy;
+
+		if (getCharTile(innerOffsetX, innerOffsetY).o === false){
+			app.gameArea.offsetY = innerOffsetY;
+		}
 	}
 
 	app.gameArea.draw();
